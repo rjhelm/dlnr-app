7296e4921e7d695583ab75358ce2dd9c094a212f
rjhelm <rjhelm@github.io>
2021-10-10 20:47:13 -0600

    adjsuted token name

---
+++ dlrn-frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      DLNR || Decentalized Learning
+      </header>
+    </div>
+  );
+}
+
+export default App;
